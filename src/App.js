b18fea8f2b3d71a9b3cb16d79e77ce44253ba76f
kodirov8788 <kodirov8788@gmail.com>
2023-04-11 22:15:36 +0500

    sdasdasd

--- src/App.js
+++ src/App.js
@@ -1,16 +1,8 @@
 import "./App.css";
-import Products from "./pages/Products/Products";
-import { useState, useContext } from "react";
-import { UserContext } from "./context/userContext"
 import { useFirebaseLogin } from "./hooks/firebaseLogin"
-import { useFirebaseStorage } from "./hooks/firebaseStorage"
-import { useFirebaseFirestore } from "./hooks/firebaseFirestore"
 
 function App() {
   const [SignOut, SignInWithGoogle, localName, localEmail, localPhoto] = useFirebaseLogin()
-  const [] = useFirebaseStorage()
-  const [userData] = useFirebaseFirestore()
-  console.log(userData);
   return (
     <div className="App">
       <h1>Home</h1>
--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,14 +1,23 @@
 import { initializeApp } from "firebase/app";
 
 
+// const firebaseConfig = {
+//     apiKey: process.env.FIREBASE_API_KEY,
+//     authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+//     projectId: process.env.FIREBASE_PROJECT_ID,
+//     storageBucket: process.env.FIREBASE_DATABASE_URL,
+//     messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+//     appId: process.env.FIREBASE_APP_ID
+// };
 const firebaseConfig = {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_DATABASE_URL,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.FIREBASE_APP_ID
+    apiKey: "AIzaSyBcpTbKOUpmG9uNEdrKMC0ZkPIyTrFI4oU",
+    authDomain: "blogs-f24cd.firebaseapp.com",
+    projectId: "blogs-f24cd",
+    storageBucket: "blogs-f24cd.appspot.com",
+    messagingSenderId: "628334680016",
+    appId: "1:628334680016:web:9a7520545cf0712c234bd5"
 };
+
 const app = initializeApp(firebaseConfig);
 
 
